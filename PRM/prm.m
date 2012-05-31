7c418cd4774ed54a2fb1be5f3a2ada85dfcc898e
Carlos Ezequiel <cfezequiel@mail.usf.edu>
2012-05-31 18:23:37 -0400

    Changed PRM grid to PRM grid-random hybrid.

--- PRM/prm.m
+++ PRM/prm.m
@@ -1,8 +1,13 @@
 % ===== Test parameters =====
 
-% Determine total number of dots as NxN
-% NOTE: at N=100, out-of-memory error was encountered
+% Initial number of configurations (NxN)
 N = 50;
+
+% Determine total number of free configurations
+nFree = 2000;
+
+% Gap between robot and obstacles
+gap = 1.5;
 
 % Start and goal points
 start = [3 4];
@@ -24,7 +29,14 @@ yStatObs = [7 111 0;
             104 208 208;
             104 208 208;
             7 111 0];
-
+        
+% Expand the obstacles to avoid the robot bumping into them
+xStatObsExp = xStatObs;
+xStatObsExp([1,2,5,6,9,10]) = xStatObsExp([1,2,5,6,9,10]) - gap;
+xStatObsExp([3,4,7,8,11,12]) = xStatObsExp([3,4,7,8,11,12]) + gap;
+yStatObsExp = yStatObs;
+yStatObsExp([1,4,5,8,9,12]) = yStatObsExp([1,4,5,8,9,12]) - gap;
+yStatObsExp([2,3,6,7,10,11]) = yStatObsExp([2,3,6,7,10,11]) + gap;
 
 % ===== Generate the configuration space =====
 
@@ -40,17 +52,27 @@ yv = yint:yint:ymax - yint;
 X = X(:);
 Y = Y(:);
 
-% Detect collisions
-IN = ones(size(X, 1), 1);
-for i = 1:size(xStatObs, 2)
-    IN = IN .* ~inpolygon(X, Y, xStatObs(:, i), yStatObs(:, i));
-end
-X = X .* IN;
-Y = Y .* IN;
-
-% Create the coordinate matrix
+% Create the configuration matrix
 C = [X'; Y']';
-C = sparse(C);
+
+% Create the free configuration matrix
+Cfree = zeros(nFree, 2);
+iC = 0;
+collision = false;
+while (iC < nFree)
+    i = randi(size(C, 1), 1);
+    c = C(i, :);
+    for j = 1:size(xStatObsExp, 2)
+        if inpolygon(c(1), c(2), xStatObsExp(:, j), yStatObsExp(:, j))
+            collision = true;
+        end
+    end
+    if ~collision
+        iC = iC + 1;
+        Cfree(iC, :) = c;
+    end
+    collision = false;
+end
 
 t1 = toc;
 
@@ -60,12 +82,12 @@ t1 = toc;
 tic;
 
 % Combine the coordinate matrix with start and end nodes
-Csg = [start(1), start(2); C; goal(1), goal(2)]; 
+Csg = [start(1), start(2); Cfree; goal(1), goal(2)]; 
 
 % Generate the distance matrix
 D = pdist2(Csg, Csg);
 
-% Get the nearest neighbor radius
+% Set the nearest neighbor radius
 r = sqrt(xint^2 + yint^2);
 
 % Set distances of 'non-near' neighbors to infinite
@@ -114,17 +136,18 @@ plot(start(1), start(2), 'or', 'MarkerSize',10)
 plot(goal(1), goal(2), 'xr', 'MarkerSize',10)
 
 % Plot the grid of free dots
-scatter(X, Y, 5/N);
+plot(Cfree(:, 1), Cfree(:, 2), '.', 'MarkerSize', 1);
 
 % Plot shortest path
 plot(Csg(path,1),Csg(path,2),'b','LineWidth',2)
 
 hold off
 
+
 % ===== Print performance results =====
 
 fprintf(' - %d collision-free samples were generated in\n %d seconds.\n\n',...
-        size(C, 1), t1)
+        size(Cfree, 1), t1)
 fprintf(' - All node distances were calculated in\n %d seconds.\n\n', t2)
 fprintf(' - Dijkstra''s Algorithm finished after\n %d seconds.\n\n', t3)
 
