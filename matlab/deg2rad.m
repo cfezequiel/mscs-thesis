2bb65132fd1ea1ecf26ecf33379a02d03e311284
Carlos Ezequiel <cfezequiel@mail.usf.edu>
2012-11-10 16:48:32 -0500

    Removed matlab gui from master branch; moved to matlab_gui_prototype branch

--- matlab/deg2rad.m
+++
@@ -1,9 +0,0 @@
-function y = deg2rad(x)
-%DEG2RAD Converts degrees to radians
-%   Input argument x is a matrix consisting of degree values. The output
-%   vector y is a matrix containing the corresponding radian values of x.
-
-y = x .* pi/180;
-
-end
-
